46a5585e2b6805e9e310c4a96013f7558a67037b
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-10-21 00:52:02 +0300

    dest file updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/Navigation/NavBar';
+import Dest from './Components/Destination/Dest';
 
 function App() {
   return (
     <div className="App">
-    <NavBar/>
+      <NavBar />
+      <Dest/>
     </div>
   );
 }
---
+++ src/Components/Destination/Dest.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Destination.css';
+import image2 from '../images/image2.jpeg'
+
+function Dest() {
+    return (
+        <section className="destionation-section">
+            <p className="destination">destination</p>
+            <p className="top-destination">top destination</p>
+
+            <div className="images">
+                <div className="box">
+                    <img src={image2} alt="hakuna picha"/>
+                </div>
+                <div className="box">
+                     <img src={image2} alt="hakuna picha"/>
+                </div>
+                <div className="box">
+                     <img src={image2} alt="hakuna picha"/>
+                </div>
+                <div className="box">
+                   <img src={image2} alt="hakuna picha"/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Dest
