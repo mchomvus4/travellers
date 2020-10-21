cf340b4b41266f4555ef7ffbe789f94552d443bc
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-10-21 13:29:10 +0300

    update
    Dest.jsx , Destination.css were updated

--- src/Components/Destination/Dest.jsx
+++ src/Components/Destination/Dest.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Destination.css';
-import image1 from '../images/image2.jpeg'
+import image1 from '../images/safari1.jpg';
+import image2 from '../images/safari2.png';
+import image3 from '../images/safari3.jpeg';
+import image4 from '../images/safari4.jpg';
 
 function Dest() {
     return (
@@ -9,18 +12,18 @@ function Dest() {
             <p className="top-destination">top destination</p>
 
             <div className="row bg-images">
-                <div className="col span-1-of-4 image">
-                    <img src={image1} alt="second" className="second" />
+                <div className="col span-1-of-4">
+                    <img src={image1} alt="Hakuna picha" className="second" />
                 </div>
 
-                <div className="col span-1-of-4 image">
-                    <img src={image1} alt="second" className="second" />
+                <div className="col span-1-of-4">
+                    <img src={image2} alt="Hakuna picha" className="second" />
                 </div>
-                <div className="col span-1-of-4 image">
-                    <img src={image1} alt="second" className="second" />
+                <div className="col span-1-of-4">
+                    <img src={image3} alt="Hakuna picha" className="second" />
                 </div>
-                <div className="col span-1-of-4 image">
-                    <img src={image1} alt="second" className="second" />
+                <div className="col span-1-of-4">
+                    <img src={image4} alt="Hakuna picha" className="second" />
                 </div>
                 
             </div>
--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './NavBar.css';
-
-
-
 function NavBar() {
     return (
        
