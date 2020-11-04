f8e7f11365b06b44ba8d9a2688723f42ccbec906
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-05 00:14:45 +0300

    file updated

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -13,13 +13,15 @@ function Team() {
                 <div className="col span-1-of-2">
                     <img src={team} alt="team-picha" className="team"/>
                 </div>
-                <div className="col span-1-of-2">
+                <div className="col span-1-of-2 bg-content-color">
                         <p className="lorem-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                          It has survived not only five centuries,
                          but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                        <a className="btn-btn-readmore" href ="#readmore">ReadMore</a>
+                        <div className="btn-readmore">
+                            <a className="btn-btn-readmore" href="#readmore">Read more</a>
+                            </div>
                     </div>
                 </div>
             </div>
