6b0ae48b2df6511db98fed4d505fb421b3848a4e
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-17 10:44:15 +0300

    file updated

--- src/Components/Activities/Activity.jsx
+++ src/Components/Activities/Activity.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import './Activity.css';
+import image1 from '../images/activity01.jpg';
+import image2 from '../images/activity02.jpg';
+import image3 from '../images/activity03.jpg';
 function Activity() {
     return (
          <section className="activities-section">
             <p className="activity">activities</p>
-            <p className="t-activities">trending activties</p>     
+            <p className="t-activities">trending activties</p>  
+            
+            <div className="row nafasi">
+                <div className="col span-1-of-3">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image1} alt="hakuna picha"/>
+                        </div>
+                        <div className="title">
+                               <h3 className="location"><i className="fa fa-map-marker map"></i>New York, NY, USA</h3>
+                        </div>
+                        <div className="des">
+                            <p>There are many variations of passages of Lorem</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                           <span className="price">$250.00</span> <button>Book Now</button>
+                        </div>
+                   </div>
+                </div>
+                <div className="col span-1-of-3">
+                     <div className="card">
+                        <div className="image">
+                            <img src={image2} alt="hakuna picha"/>
+                        </div>
+                        <div className="title">
+                               <h3 className="location"><i className="fa fa-map-marker map"></i>Paris, FRANCE</h3>
+                        </div>
+                        <div className="des">
+                            <p>There are many variations of passages of Lorem</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                           <span className="price">$250.00</span> <button>Book Now</button>
+                        </div>
+                   </div>
+                </div>
+
+                <div className="col span-1-of-3">
+                     <div className="card">
+                        <div className="image">
+                            <img src={image3} alt="hakuna picha"/>
+                        </div>
+                        <div className="title">
+                               <h3 className="location"><i className="fa fa-map-marker map"></i>London, England</h3>
+                        </div>
+                        <div className="des">
+                            <p>There are many variations of passages of Lorem</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                           <span className="price">$250.00</span> <button>Book Now</button>
+                        </div>
+                   </div>
+                </div>
+            </div>
+
            
         </section>
     )
