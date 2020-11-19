dc9f5836937fd362c314db56785718815128fd84
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-19 15:57:02 +0300

    testimonial file created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './Components/Navigation/NavBar';
 import Dest from './Components/Destination/Dest';
 import Team from './Components/Team/Team';
 import Activity from './Components/Activities/Activity';
+import Testimonial from './Components/Testimonial/Testimonial';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <NavBar />
       <Dest />
       <Team />
-      <Activity/>
+      <Activity />
+      <Testimonial/>
     </div>
   );
 }
---
+++ src/Components/Testimonial/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Testimonial.css';
+const Testimonial = () => {
+    return ( 
+         <section className="testimonial-section">
+           <p className="testimonial">testimonials</p>
+           <p className="c-testimonial">client testmonials </p> 
+        </section>
+     );
+}
+ 
+export default Testimonial;
