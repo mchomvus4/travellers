1fdcfcba8755cfa3f5f7c31126f3fdce5a644624
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-21 10:31:18 +0300

    new changes

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
 import './Testimonial.css';
+import image from '../images/sam.png'
 const Testimonial = () => {
     return ( 
          <section className="testimonial-section">
            <p className="testimonial">testimonials</p>
            <p className="c-testimonial">client testmonials </p> 
+
+            <div className="testimonials">
+                <div className="inner">
+                    <div className="row main">
+                        <div className="column">
+                            <div className="test">
+                                <img src={image} alt="hakuna picha" />
+                                <div className="name">Samwel Mchomvu</div>
+                                <div className="star">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                            </div>
+                        </div>
+
+                         <div className="column">
+                            <div className="test">
+                                <img src={image} alt="hakuna picha" />
+                                <div className="name">Samwel Mchomvu</div>
+                                <div className="star">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                            </div>
+                        </div>
+
+                         <div className="column">
+                            <div className="test">
+                                <img src={image} alt="hakuna picha" />
+                                <div className="name">Samwel Mchomvu</div>
+                                <div className="star">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </section>
      );
 }
