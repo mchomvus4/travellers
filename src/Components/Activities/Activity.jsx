e451c1b363858d8ff9825929f1140f7647cbccac
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-22 21:57:50 +0300

    new files updates

--- src/Components/Activities/Activity.jsx
+++ src/Components/Activities/Activity.jsx
@@ -8,61 +8,53 @@ function Activity() {
          <section className="activities-section">
             <p className="activity">activities</p>
             <p className="t-activities">trending activties</p>  
+            <div className="row nafasi">
+                <div className="col span-1-of-3 card-main">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image1} alt="hakuna picha"/>
+                        </div>
+                        <div className="title">
+                               <h3 className="location"><i className="fa fa-map-marker map"></i>New York, NY, USA</h3>
+                        </div>
+                        <div className="des">
+                            <p className="para1"><b>There are many variations <br/>of passages of Lorem</b></p>
+                            <p className="para2">Lorem Ipsum is simply dummy text of<br/> the printing and typesetting industry<br/> Lorem Ipsum standard</p>
+                           <span className="price">$250.00</span> <button>Book Now</button>
+                        </div>
+                   </div>
+                </div>
+               
+               <div className="col span-1-of-3 card-main">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image2} alt="hakuna picha"/>
+                        </div>
+                        <div className="title">
+                               <h3 className="location"><i className="fa fa-map-marker map"></i>Paris, FRANCE</h3>
+                        </div>
+                        <div className="des">
+                            <p className="para1"><b>There are many variations <br/>of passages of Lorem</b></p>
+                            <p className="para2">Lorem Ipsum is simply dummy text of<br/> the printing and typesetting industry<br/> Lorem Ipsum standard</p>
+                           <span className="price">$250.00</span> <button>Book Now</button>
+                        </div>
+                   </div>
+                </div>
 
-            <div className="act">
-                <div className="inner">
-                    <div className="row activity-row">
-                        <div className="column-activity">
-                            <div className="activities">
-                            <img src={image1} alt="hakuna picha" />
-                            <div className="location">
-                                <i class="fa fa-map-marker map"></i>
-                                 <p className="l-name">New York, NY, USA</p>
-                            </div>
-                            <p className="para1"><b>There are many variations of<br/> passages of Lorem</b></p>
-                            <p className="para2">Lorem Ipsum is simply dummy text of <br/>the printing and typesetting industry.<br/>Lorem Ipsum standard</p>
-                            </div>
-                            <div className="border"></div>
-                            <div className="price">
-                            <p className="cost">$250.00</p>
-                            <button>Book Now</button>
+                <div className="col span-1-of-3 card-main">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image3} alt="hakuna picha"/>
                         </div>
+                        <div className="title">
+                               <h3 className="location"><i className="fa fa-map-marker map"></i>London, England</h3>
                         </div>
-
-                        <div className="column-activity">
-                            <div className="activities">
-                            <img src={image2} alt="hakuna picha" />
-                            <div className="location">
-                                <i class="fa fa-map-marker map"></i>
-                                 <p className="l-name">Paris, FRANCE</p>
-                            </div>
-                            <p className="para1"><b>There are many variations of<br/> passages of Lorem</b></p>
-                            <p className="para2">Lorem Ipsum is simply dummy text of <br/>the printing and typesetting industry.<br/>Lorem Ipsum standard</p>
-                            </div>
-                             <div className="price">
-                            <p className="cost">$250.00</p>
-                            <button>Book Now</button>
+                        <div className="des">
+                            <p className="para1"><b>There are many variations <br/>of passages of Lorem</b></p>
+                            <p className="para2">Lorem Ipsum is simply dummy text of<br/> the printing and typesetting industry<br/> Lorem Ipsum standard</p>
+                           <span className="price">$250.00</span> <button>Book Now</button>
                         </div>
-                        </div>
-                        
-
-                        <div className="column-activity">
-                            <div className="activities">
-                            <img src={image3} alt="hakuna picha" />
-                            <div className="location">
-                                <i class="fa fa-map-marker map"></i>
-                                 <p className="l-name">London,England</p>
-                            </div>
-                            <p className="para1"><b>There are many variations of<br/> passages of Lorem</b></p>
-                            <p className="para2">Lorem Ipsum is simply dummy text of <br/>the printing and typesetting industry.<br/>Lorem Ipsum standard</p>
-                            </div>
-                             <div className="price">
-                            <p className="cost">$250.00</p>
-                            <button>Book Now</button>
-                        </div>
-                        </div>
-                        
-                    </div>
+                   </div>
                 </div>
             </div>
         </section>
